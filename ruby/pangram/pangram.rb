3019d3f06389204299ce917bfc70bcc62dbfa3dc
Angela Dyrda <adyrda2@gmail.com>
2016-11-18 08:38:04 -0600

    Update pangram.rb

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,10 +1,10 @@
 class Pangram
-	def self.is_pangram?(str)
-		letters = ("a".."z").to_a
-		letters.all? { |char| str.downcase.include?(char) } ? true : false
-	end 
+  def self.is_pangram?(str)
+    letters = ("a".."z").to_a
+    letters.all? { |char| str.downcase.include?(char) } ? true : false
+  end 
 end 
 
 class BookKeeping
-	VERSION = 2
+  VERSION = 2
 end 
