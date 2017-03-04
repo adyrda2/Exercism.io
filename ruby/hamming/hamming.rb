429c161aca74a9bee1353c4db4605e9c04b52d19
Angela Dyrda <adyrda2@gmail.com>
2017-03-03 21:55:28 -0600

    Update hamming.rb

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,24 +1,5 @@
 
 class Hamming
-  VERSION = 1
-  def self.compute(string_one, string_two)
-    if string_one.length != string_two.length 
-      raise ArgumentError
-    end 
-      index = 0 
-      counter = 0 
-    while (string_one[index]) && (string_two[index])
-      if string_one[index] != string_two[index]
-	counter += 1 
-      end 
-      index +=1 
-    end
-    return counter 
-  end 
-end
-
-#solution 2
-class Hamming
   VERSION = 2
   def self.compute(string_one, string_two)
     raise ArgumentError if string_one.length != string_two.length
